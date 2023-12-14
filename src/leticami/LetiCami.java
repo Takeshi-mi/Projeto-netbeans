c9a6ea37065740aca3f549dc929cceaa9d1990d6
Takeshi Miura <101356765+Takeshi-mi@users.noreply.github.com>
2023-12-14 11:18:22 -0300

    batata

---
+++ src/leticami/LetiCami.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package leticami;
+
+/**
+ *
+ * @author Takeshi
+ */
+public class LetiCami {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("uhuu to aprendendo essa bagaça");
+    }
+    
+}
