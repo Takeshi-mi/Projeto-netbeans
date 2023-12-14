ed614a948b2c50f9272a9713cc0c9ef8240ada2f
Letícia Bittencourt Vieira <128814175+bittecourt@users.noreply.github.com>
2023-12-14 11:30:02 -0300

    alterei o print

--- src/leticami/LetiCami.java
+++ src/leticami/LetiCami.java
@@ -14,7 +14,7 @@ public class LetiCami {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("uhuu to aprendendo essa bagaça");
+        System.out.println("uhuu to aprendendo essa bagaça , :)");
     }
     
 }
